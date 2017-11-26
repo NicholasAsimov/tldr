5cd252350c13a43085308b63b248dca6e372de5c
Nicholas Asimov <nicholas@asimov.me>
2017-11-26 07:24:29 +0300

    Add wip markdown rendering
    Related: #2

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const baseURL = "https://raw.githubusercontent.com/tldr-pages/tldr/master/pages/common/%s.md"
 
+const (
+	ANSI_CLEAR     = "\033[0m"
+	ANSI_BOLD      = "\033[1m"
+	ANSI_UNDERLINE = "\033[4m"
+	INDENT         = "  "
+)
+
+// TODO add caching
 func getPage(name string) (io.Reader, error) {
 	url := fmt.Sprintf(baseURL, name)
 
@@ -18,6 +28,50 @@ func getPage(name string) (io.Reader, error) {
 	}
 
 	return resp.Body, nil
+}
+
+func render(page io.Reader) error {
+	contentRaw, err := ioutil.ReadAll(page)
+	if err != nil {
+		return fmt.Errorf("can't read page: %s", err)
+	}
+
+	// Print initial newline
+	fmt.Printf("\n")
+
+	lines := strings.Split(string(contentRaw), "\n")
+
+	for _, line := range lines {
+		// fmt.Printf("<%q>", line)
+		if line == "" {
+			fmt.Printf("\n")
+			continue
+		}
+
+		block := line[0]
+		text := line[2:len(line)]
+
+		fmt.Printf(INDENT)
+
+		switch block {
+		// Command name
+		case '#':
+			fmt.Println(ANSI_UNDERLINE + ANSI_BOLD + text + ANSI_CLEAR)
+		// Command description
+		case '>':
+			fmt.Println(ANSI_BOLD + text + ANSI_CLEAR)
+			// Example description
+		case '-':
+			fmt.Printf(line)
+		// Example code
+		// case '`':
+		// 	fmt.Println(line)
+		default:
+			fmt.Println(line)
+		}
+	}
+
+	return nil
 }
 
 func main() {
@@ -38,8 +92,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = io.Copy(os.Stdout, page)
-	if err != nil {
+	if err := render(page); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
